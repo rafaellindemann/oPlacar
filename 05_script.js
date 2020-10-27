b95ea999c6aa43b3ee819fac7171e0ac7cbb9621
RAFAEL LINDEMANN DUARTE <69912539+rafaellindemann@users.noreply.github.com>
2020-10-26 22:25:12 -0300

    Requisitos funcionais checkados, estilos aprimorados
    Colocamos radios e checbox scss;
    Definimos o primeiro tema;
    Muitas outras pequenas coisas que já não lembro mais...

--- 05_script.js
+++ 05_script.js
@@ -1,35 +1,50 @@
 ///////////////////// Variáveis Globais /////////////////////
 var placarEsquerda = 0, placarDireita = 0, vitoriaEsquerda = 0, vitoriaDireita = 0
-
+var menuAMostra = false
 var nomeEsquerda = document.getElementById("botaoNomeEsquerda").textContent
 var nomeDireita = document.getElementById("botaoNomeDireita").textContent
 var permitidoNegativo
 var pontuacaoFimDoJogo = 5
 
-
+console.log(window)
 /////////////////////////////////////////////////////////////
 
 ////////////////////////// Funções do visual //////////////////////////
 
-var MenuLateral = document.getElementById("divMenu");
+var MenuLateral = document.getElementById("divMenuLateral");
 var clique_Menu = document.getElementById("btnMenuLateral");
+var Botao = document.getElementById("divBotaoMenu")
+
 var mensagemFinalDePartida = document.getElementById('divMensagemFinalDePartida'); // mensagem de vencedor
 var painelFinalDePartida = document.getElementById('divPainelFinalDePartida') // quadro voador do LeoSilva
 
 var Body = document.getElementById("Body")
+var clique_Radio = document.getElementById("rdoVisual1")
 
-clique_Menu.onclick = function (e) {
+clique_Menu.onclick = function (e) { // isso abre o menuLateral
     e.preventDefault();
+    if (menuAMostra == false){
     MenuLateral.classList.toggle('toggleMenuEsquerda');
+
+    menuAMostra = true
+    Botao.style = 'display: none;'
+    console.log(menuAMostra)
+    } 
 }
 
-function mudarTema(e) {
-    // document.getElementsByTagName('body')[0].setAttribute("class", "Visual1");
-    // alert('Teste')
+
+
+clique_Radio.onchange = function (e) {
+    var teste = document.getElementsByTagName('input')
+    for(i=0; i<teste.length; i++)
+    {
+        teste[i].style.setProperty('--border-hover', 'blue')
+    }
     e.preventDefault()
     Body.classList.toggle('Visual1');
 
-    alert("é")
+    // document.getElementsByTagName('body')[0].setAttribute("class", "Visual1");
+    // alert("é")
 }
 
 /////////////////////////////////////////////////////////////
@@ -165,6 +180,8 @@ function mudarLados() {
 }
 
 function zerarPlacar() {
+
+    // console.log(painelFinalDePartida.) // *** teste pra achar o z
     placarEsquerda = 0
     placarDireita = 0
 
@@ -211,3 +228,38 @@ function avisaVencedor(vencedor) {
 function abrirMenu() {
 
 }
+
+// -- TESTE PARA ESCONDER O MENU CLICANDO FORA --
+
+
+console.log(menuAMostra)
+// 
+var menu = document.getElementById('divMenuLateral')
+var html = document.body.parentNode
+
+html.onclick = function (event) {
+    // console.log(event)
+    // console.log(event.path[1].id == "btnMenuLateral")
+    // if(event.path[1].id == "btnMenuLateral") alert('To aqui, porraaaaaaa')
+    if (menuAMostra) {
+        // event.preventDefault();
+        if (!menu.contains(event.target) && event.path[1].id != "btnMenuLateral") {
+            // alert('Clicked in the página')
+            menuAMostra = false
+            console.log(menuAMostra)
+            // *** fechar o menu lateral
+            MenuLateral.classList.toggle('toggleMenuEsquerda');
+            
+            Botao.style = 'display: inline;'
+            var meioDaPagina = window.innerWidth/2 - 200
+            // vai!
+            document.getElementById("divMenuLateral").style.margin = `${meioDaPagina}px;`
+            
+            console.log(window)
+        } 
+    }
+}
+
+
+
+// 
